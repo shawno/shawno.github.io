2c400c171ece5a0a311001088419dd01cb3a5d4b
Shawn O'Neill <soneill83@gmail.com>
2014-01-21 22:47:44 -0800

    testing hbs on resume

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+$( document ).ready(function() {
+    var source = $("#job-template").html();
+    var template = Handlebars.compile(source);
+    alert(template);
+    $("#jobs-target").html(template);    
+});
 
---
+++ resume/jobs.js
@@ -0,0 +1,30 @@
+var jobs =  [
+        {
+            "company" : "Mountain Equipment Co-op",
+            "position" : "Senior Software Developer - e-commerce",
+            "startDate" : "March, 2013",
+            "endDate" : "",
+            "ongoing" : "true",
+            "location" : "Vancouver, BC",
+            "responsibilities" : "lorem ipsum etc etc"
+        },
+        {
+            "company" : "Funl",
+            "position" : "Co-Founder",
+            "startDate" : "November, 2011",
+            "endDate" : "",
+            "ongoing" : "true",
+            "location" : "Vancouver, BC",
+            "responsibilities" : "Designed and developed a mobile-first, responsive web app for setting personal goals and building good habits to reach them. Created company vision and strategy while managing design, development, marketing, funding, and support."
+        },
+        {
+            "company" : "Verafin",
+            "position" : "Senior Software Engineer",
+            "startDate" : "August, 2010",
+            "endDate" : "August, 2012",
+            "ongoing" : "false",
+            "location" : "Vancouver, BC",
+            "responsibilities" : "Managed a regulatory reporting project from research to release. Tasks included customer interviews, design, prototyping, user testing, implementation, QA and rollout. Collaborated closely with customers, users, and regulators externally, as well as the product owner, development, support, training, and documentation teams internally."
+            
+        }
+    ];
