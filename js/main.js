993d7e31328085148b86b170c9869647db1d60b3
Shawn O'Neill <soneill83@gmail.com>
2014-01-21 22:53:38 -0800

    Fixed missing end script tag. Moved main js into index to force synchronous execution for meow

--- js/main.js
+++ js/main.js
@@ -1,7 +1,2 @@
-$( document ).ready(function() {
-    var source = $("#job-template").html();
-    var template = Handlebars.compile(source);
-    alert(template);
-    $("#jobs-target").html(template);    
-});
+
 
