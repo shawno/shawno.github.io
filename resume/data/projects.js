94a801c416ef4d804615fbf3a9c27f857cc6faa6
Shawn O'Neill <soneill83@gmail.com>
2016-02-05 21:03:48 -0800

    preparing for printing

--- resume/data/projects.js
+++ resume/data/projects.js
@@ -18,13 +18,13 @@ var projectsVar =  {
             "intro"     : [ "Developed an open source, minimalist bike blogging platform to capture my ride across Canada in the summer of 2015."], 
             "tasks"     : [ "Built with Ruby on Rails, Strava API, Google Maps API, and Instagram." ]
         }
-        ,{
-            "title"     : "Emily Schmid - Designer",
-            "date"      : "Winter 2015",
-            "url"       : "http://emilyschmid.com",
-            "urlTitle"  : "EmilySchmid.com",
-            "intro"     : [ "Simple brochure site for a Vancouver fashion designer. Powered by Jekyll, hosted on Github Pages."]
-        }
+        // ,{
+        //     "title"     : "Emily Schmid - Designer",
+        //     "date"      : "Winter 2015",
+        //     "url"       : "http://emilyschmid.com",
+        //     "urlTitle"  : "EmilySchmid.com",
+        //     "intro"     : [ "Simple brochure site for a Vancouver fashion designer. Powered by Jekyll, hosted on Github Pages."]
+        // }
         ,{
             "title"     : "AlleyCat - Mobify Hackathon Winner",
             "date"      : "Winter 2013",
@@ -37,15 +37,15 @@ var projectsVar =  {
             "date"   : "Fall 2012",
             "intro"  : [ "Led a team of nine through the formation of an expense-tracking startup venture. Deliverables included branding, design, marketing strategy, and a functional iOS app." ] 
         }
-        ,{
-            "title"  : "Eye-tracking Assistive Technology",
-            "date"   : "Spring 2008",
-            "intro"  : [ "Built a pupil-tracking cursor interaction system for users with complete paralysis." ]
-        },
-        {
-            "title"  : "Gesture-tracking Presentation Aid",
-            "date"   : "Fall 2007",
-            "intro"  : [ "Used a standard webcam and skin-tone segmenetation in HSV colour space to track a presenter's hand gestures and control a slide show." ]
-        }
+        // ,{
+        //     "title"  : "Eye-tracking Assistive Technology",
+        //     "date"   : "Spring 2008",
+        //     "intro"  : [ "Built a pupil-tracking cursor interaction system for users with complete paralysis." ]
+        // },
+        // {
+        //     "title"  : "Gesture-tracking Presentation Aid",
+        //     "date"   : "Fall 2007",
+        //     "intro"  : [ "Used a standard webcam and skin-tone segmenetation in HSV colour space to track a presenter's hand gestures and control a slide show." ]
+        // }
     ]
 };
